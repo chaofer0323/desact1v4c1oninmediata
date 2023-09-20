a80f0c959a1df4cbfa9a20e676fc1e04c66c9886
chaofer0323 <145617533+chaofer0323@users.noreply.github.com>
2023-09-20 16:46:53 -0500

    Add files via upload

---
+++ metronic/include/env.php
@@ -0,0 +1,9 @@
+<?php
+$env = [
+    'DB_HOST' => 'containers-us-west-101.railway.app',
+    'DB_NAME' => 'railway',
+    'DB_USER' => 'root',
+    'DB_PASS' => 'byJB8TOlJq3RBpo7KIeb',
+    'DB_PORT' => '5916'
+];
+?>
